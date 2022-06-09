931d2ad7a96e6e7ea6d5142a59cdb2c678dc9724
Dmitry Bagryanskiy <9212837+bdvme@users.noreply.github.com>
2022-06-09 18:00:29 +0300

    Add homework 5.5 task 1 and little bit task 2

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,19 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
+# Узнать командой: yc config list | grep cloud-id
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1guptrv7v7c8mtp5kof"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
+# Узнать командой: yc config list | grep folder-id
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1g9s0cv770m9p5ei3n1"
 }
 
 # Заменить на ID своего образа
-# ID можно узнать с помощью команды yc compute image list
+# ID можно узнать с помощью команды yc compute image list --format yaml | grep -w id:
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd8juo8ak9e5bdsbduu8"
 }
